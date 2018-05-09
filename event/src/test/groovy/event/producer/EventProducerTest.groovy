179f593d2597d608b97a01d6ae3c1994b93eec80
Ajeet Bansal <ajeet.aggregatortech@gmail.com>
2018-05-09 15:28:25 -0700

    Event Producer

--- event/src/test/groovy/event/producer/EventProducerTest.groovy
+++ event/src/test/groovy/event/producer/EventProducerTest.groovy
@@ -1,20 +1,39 @@
 package event.producer
 
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerRecord
+import spock.lang.Shared
 import spock.lang.Specification
 
 import static org.hamcrest.MatcherAssert.assertThat
 import static org.hamcrest.Matchers.equalTo
 
 class EventProducerTest extends Specification {
-    def 'Test Publish Event()'() {
+    @Shared
+    String eventPayload = "payload"
+
+    def 'Test Publish Event'() {
+        setup:
+        KafkaProducer<String,String> mockKafkaProducer = Mock()
+        EventProducer spyEventProducer = Spy(EventProducer)
+
+        when:
+        spyEventProducer.buildKafkaProducer() >> mockKafkaProducer
+        Boolean result = spyEventProducer.publish(eventPayload)
+
+        then:
+        1 * mockKafkaProducer.send({it.key ==  eventPayload && it.value == eventPayload})
+        assertThat(result, equalTo(Boolean.TRUE))
+    }
+
+    def 'Test buildKafkaProducer'() {
         setup:
         EventProducer eventProducer = new EventProducer()
 
         when:
-        String eventPayload = "payload"
-        Boolean result = eventProducer.publish(eventPayload)
+        KafkaProducer<String, String> kafkaProducer = eventProducer.buildKafkaProducer()
 
         then:
-        assertThat(result, equalTo(Boolean.TRUE))
+        kafkaProducer != null
     }
 }
