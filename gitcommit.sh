54151c2bfbdf02f3ab0fe7a8b39ff66e150f0636
Atul-aggregatortech <38672199+Atul-aggregatortech@users.noreply.github.com>
2018-05-14 15:56:17 -0700

    Update gitcommit.sh

--- gitcommit.sh
+++ gitcommit.sh
@@ -1,6 +1,6 @@
 #cd ..
 #rm -rf heroku-platform
-ssh -T git@github.com
+#ssh -T git@github.com
 #git clone git://github.com/aggregatortech/Aggregator-Tech/heroku-platform
 echo 'Path is ${PWD}'
 #cd ..
@@ -15,7 +15,7 @@ git config --global user.email "atul.aggregatortech@gmail.com"
 #git config --global http.sslverify false
 git config --global http.proxy http://www-proxy.us.oracle.com:80
 git config --global https.proxy http://www-proxy.us.oracle.com:80
-#git remote add origin https://atul-aggregatortech:Westworld1@github.com/aggregatortech/Aggregator-Tech/platform
+git remote add origin https://atul-aggregatortech:Westworld1@github.com/aggregatortech/Aggregator-Tech/platform
 git checkout master
 touch commitFile
 git add -A
