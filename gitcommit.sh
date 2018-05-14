bc47a68970b5201d055e0143aceda8caa26d96d0
Atul-aggregatortech <38672199+Atul-aggregatortech@users.noreply.github.com>
2018-05-14 11:02:19 -0700

    Create gitcommit.sh

---
+++ gitcommit.sh
@@ -0,0 +1,11 @@
+cd ..
+echo ${GIT_USERNAME}
+echo ${GIT_PASSWORD}
+git config --global user.name "atul-aggregatortech"
+git config --global user.email "atul.aggregatortech@gmail.com"
+git config --global http.sslverify false
+git clone https://github.com/Aggregator-Tech/heroku-platform
+touch build.gradle
+git add --all
+git commit -m "Add change "
+git push origin master
