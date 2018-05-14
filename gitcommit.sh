cebf64cb869301e89be65f91bc7d3968b36eb682
Atul-aggregatortech <38672199+Atul-aggregatortech@users.noreply.github.com>
2018-05-14 15:14:28 -0700

    Update gitcommit.sh

--- gitcommit.sh
+++ gitcommit.sh
@@ -12,7 +12,7 @@ echo ${GIT_PASSWORD}
 #git config --global user.name "atul-aggregatortech"
 #git config --global user.email "atul.aggregatortech@gmail.com"
 #git config --global http.sslverify false
-git config --global http.proxy http://www-proxy.yourcompany.com:80
+git config --global http.proxy http://www-proxy.us.oracle.com:80
 git checkout master
 touch commitFile
 git add -A
