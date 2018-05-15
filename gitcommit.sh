3a265ae4b783d82b4ea2477467bd449a81ffc026
aggregatortech <38509708+aggregatortech@users.noreply.github.com>
2018-05-15 16:49:48 -0700

    Update gitcommit.sh

--- gitcommit.sh
+++ gitcommit.sh
@@ -1,27 +1,10 @@
-#cd ..
-#rm -rf heroku-platform
-#ssh -T git@github.com
-
-#echo 'Path is ${PWD}'
-
-echo 'Path is  ${PWD}'
-rm -rf heroku-platform
-#git remote set-url origin https://github.com/Aggregator-Tech/heroku-platform
-#git remote add origin https://github.com/Aggregator-Tech/heroku-platform
-#git config --global credential.helper 'store --file /var/jenkins_home/git.store'
-#git config --global user.name "aggregatortech"
-#git config --global user.email "aggregatortech@gmail.com"
-#git config --global http.sslverify false
-#git config --global http.proxy http://www-proxy.us.oracle.com:80
-#git config --global https.proxy http://www-proxy.us.oracle.com:80
-git init
-git clone https://github.com/Aggregator-Tech/heroku-platform
-git remote add origin https://github.com/Aggregator-Tech/heroku-platform
-
-
 #git checkout master
 #git pull
 echo "new build" >> heroku-platform/commitFile
+git status
 git add -A
+git status
 git commit -am "my update msg"
-git push origin HEAD:master
+git status
+git push
+git status
