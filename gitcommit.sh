c3413c2a4d5397affde4436780c3c3d799cd5628
aggregatortech <38509708+aggregatortech@users.noreply.github.com>
2018-05-15 18:15:07 -0700

    Update gitcommit.sh

--- gitcommit.sh
+++ gitcommit.sh
@@ -1,10 +1,7 @@
-#git checkout master
-#git pull
+git config --global user.name "aggregatortech"
+git config --global user.email "aggregatortech@gmail.com"
 echo "new build" >> commitFile
-git status
 git add -A
-git status
 git commit -am "my update msg"
-git status
+git config --global credential.helper 'store --file /var/jenkins_home/git.store
 git push
-git status
