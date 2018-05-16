1977466928140cd48c462ed7bcb615c310db5cc9
aggregatortech <38509708+aggregatortech@users.noreply.github.com>
2018-05-15 18:15:55 -0700

    Update gitcommit.sh

--- gitcommit.sh
+++ gitcommit.sh
@@ -3,5 +3,5 @@ git config --global user.email "aggregatortech@gmail.com"
 echo "new build" >> commitFile
 git add -A
 git commit -am "my update msg"
-git config --global credential.helper 'store --file /var/jenkins_home/git.store
+git config --global credential.helper 'store --file /var/jenkins_home/git.store'
 git push
