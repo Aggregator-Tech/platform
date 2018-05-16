ee1f0b67c4121baf294a4c12b0eff1421a9d4aaa
aggregatortech <38509708+aggregatortech@users.noreply.github.com>
2018-05-15 18:40:46 -0700

    Update gitcommit.sh

--- gitcommit.sh
+++ gitcommit.sh
@@ -1,6 +1,6 @@
 git config --global user.name "aggregatortech"
 git config --global user.email "aggregatortech@gmail.com"
-echo "new build" >> commitFile
+cp ../../webTemplate/build/install/webTemplate/lib/webTemplate-1.0.jar deployable-artifacts/
 git add -A
 git commit -am "my update msg"
 git config --global credential.helper 'store --file /var/jenkins_home/git.store'
