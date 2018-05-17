933d477d39fc449abac65df07246d8b3677a170b
aggregatortech <38509708+aggregatortech@users.noreply.github.com>
2018-05-17 11:37:13 -0700

    Update AboutTest.groovy

--- integ/src/test/groovy/webTemplate/resource/AboutTest.groovy
+++ integ/src/test/groovy/webTemplate/resource/AboutTest.groovy
@@ -16,8 +16,11 @@ class AboutTest extends Specification{
     def testGetAbout() {
         setup:
         ClientBuilder clientBuilder = ClientBuilder.newBuilder();
+        println "baseUrl= $basurl"
+        
         WebTarget target = clientBuilder.build().target(baseUrl).path("/v1/about")
-
+        String path =  target.getUri().getPath()
+        println "Path is $path "
         when:
         String response = target.request().get(String.class)
         println "response = $response"
