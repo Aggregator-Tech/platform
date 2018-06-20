6d8fe109d9d4d5ec41b25b257b5d4fed6534dab3
Ajeet Bansal <ajeet.aggregatortech@gmail.com>
2018-06-20 13:16:29 -0700

    Created BaseSpecification

--- data/src/integrationTest/groovy/platform/data/provider/RedisKeyValueProviderFactoryTest.groovy
+++ data/src/integrationTest/groovy/platform/data/provider/RedisKeyValueProviderFactoryTest.groovy
@@ -3,18 +3,15 @@ package platform.data.provider
 import org.glassfish.hk2.api.ServiceLocator
 import platform.common.ServiceLocatorHelper
 import platform.common.io.system.SystemHelper
+import platform.common.test.BaseSpecification
 import platform.data.DataConfigProperty
 import platform.data.constant.DataRepositoryType
-import platform.data.provider.impl.RedisKeyValueProviderFactory
 import spock.lang.Shared
-import spock.lang.Specification
 
-class RedisKeyValueProviderFactoryTest extends Specification{
+class RedisKeyValueProviderFactoryTest extends BaseSpecification{
 
-    @Shared ServiceLocator serviceLocator;
-
-    def setup() {
-        serviceLocator = ServiceLocatorHelper.serviceLocator;
+    def setupSpec() {
+        println "setup RedisKeyValueProviderFactoryTest";
     }
     def "Verify Set and Get entry in Redis" () {
         setup:
